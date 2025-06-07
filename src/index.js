9ad72d3dc8e580a872938e567faf02a335b52273
bhaskar.Garikapati <baschow2009@gmail.com>
2025-06-07 22:16:28 +0530

    feature/FB-0000-Added Material UI

--- src/index.js
+++ src/index.js
@@ -2,10 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2', // MUI's default primary color
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
